6f3f42867202f0a08b02b678db921d57b756dd21
Rashad786 <mohdrashad522@gmail.com>
2025-08-04 18:41:58 +0530

    Add files via upload

---
+++ binary_str.java
@@ -0,0 +1,20 @@
+package Recursion;
+
+public class binary_str {
+    public static void printBinString(int n,int lastplace,String str){
+        if(n == 0){
+            System.out.println(str);
+            return;
+        }
+
+        //kaam
+        printBinString(n-1, 0, str+'0');
+
+        if(lastplace == 0){
+            printBinString(n-1, 1, str+'1');
+        }
+    }
+    public static void main(String[] args) {
+        printBinString(3, 0, "");
+    }
+}
